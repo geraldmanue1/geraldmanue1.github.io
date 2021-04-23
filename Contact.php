cfd044cff0a953a4ec8ade4cbcec143b70bf9d3a
MANUEL Gérald <geraldmanuel96@gmail.com>
2021-04-23 23:28:31 +0400

    contact

--- Contact.php
+++ Contact.php
@@ -1,74 +1,37 @@
 <?php
+require 'vendor/autoload.php';
 
-	$email= "geraldmanuel96@gmail.com";  /*****ENTER_FROM_EMAIL_ADDRESS*****/
-	$name= "MANUEL Gerald";				/*****ENTER_A_NAME*****/
-	$body= "Bienvenue dans mon premier email sendgrid ....!";
-	$subject= "Premier e-mail sendgrid";
+$API_KEY = "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";
 
+if(iset($_POST['sendmail']))
+{
+	$lastname = $_POST['last-name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$message = $_POST['message'];
 
-	$headers= array(
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("geraldmanuel96@gmail.com", "MANUEL");
+// $email->setSubject("");
+$email->addTo($email, $lastname);
+$email->addContent("text/plain", $message);
+// $email->addContent(
+   //  "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+// );
+$sendgrid = new \SendGrid(getenv($API_KEY);
 
-			'Authorization: Bearer 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',  /*****ENTER_YOUR_API_KEY*****/
-			'Content-Type: application/json'
-	);
+if($sendgrid->send($email));
+{
+	echo "Email envoie succès";
+}
 
-
-
-	$data = array(
-
-			"personalizations" => array(
-
-				array(
-
-					"to" =>array(
-
-						array(
-
-							"email" =>"geraldmanuel96@gmail.com", /*****ENTER_TO_EMAIL_ADDRESS*****/
-							"name"  => $name
-						)
-					)
-				)
-
-			),
-
-
-			"from" => array(
-
-				"email"=> $email
-			),
-
-
-			"subject" =>$subject,
-			"content" =>array(
-
-					array(
-
-						"type" => "text/html",
-						"value" => $body
-					)
-			)
-
-
-	);
-
-
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	
-	$response = curl_exec($ch);
-
-	curl_close($ch);
-
-	echo $response;
-
-
-
-
+//try {
+  //  $response = $sendgrid->send($email);
+   // print $response->statusCode() . "\n";
+   // print_r($response->headers());
+   // print $response->body() . "\n";
+//} catch (Exception $e) {
+  //  echo 'Caught exception: '. $e->getMessage() ."\n";
+//}
+//}
 ?>
