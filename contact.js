e651fc920d1001215bfe45525aabe6ac07b316ee
geraldmanue1 <61378822+geraldmanue1@users.noreply.github.com>
2022-11-03 21:38:01 +0400

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,7 +1,7 @@
 
 /* Application Firebase */
 
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD5qHZfmt-NSgWmwnxa8YhSA7Ycfp8-SPM",
   authDomain: "contactform-704d0.firebaseapp.com",
@@ -13,57 +13,57 @@ const firebaseConfig = {
   measurementId: "G-86T45ZLB8D"
 };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig); 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Reference messages collection
- var messagesRef = firebase.database().ref('messages');
-  
-  // Listen for form submit
-  document.getElementById('contactForm').addEventListener('submit', submitForm);
-  
-  // Submit form
-  function submitForm(e){
-    e.preventDefault();
- 
-  
-    
-    // Get values
-    var firstname = getInputVal('firstname');
-    var lastname = getInputVal('lastname');
-    var email = getInputVal('email');
-    var phone = getInputVal('phone');
-    var message = getInputVal('message');
-    
-  
-    // Save message
-    saveMessage(firstname, lastname, email, phone, message);
-  
-    // Show alert
-    document.querySelector('.alert').style.display = 'block';
-  
-    // Hide alert after 3 seconds
-    setTimeout(function(){
-      document.querySelector('.alert').style.display = 'none';
-    },3000);
-  
-    // Clear form
-    document.getElementById('contactForm').reset();
-  }
-  
-  // Function to get get form values
-  function getInputVal(id){
-    return document.getElementById(id).value;
-  }
-  
-  // Save message to firebase
-  function saveMessage(firstname, lastname, email, phone, message){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-      firstname:firstname,
-      lastname:lastname,
-      email:email,
-      phone:phone,
-      message:message
-    });
-  }
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+  e.preventDefault();
+
+
+
+  // Get values
+  var firstname = getInputVal('firstname');
+  var lastname = getInputVal('lastname');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+
+  // Save message
+  saveMessage(firstname, lastname, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function () {
+    document.querySelector('.alert').style.display = 'none';
+  }, 3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(firstname, lastname, email, phone, message) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    phone: phone,
+    message: message
+  });
+}
